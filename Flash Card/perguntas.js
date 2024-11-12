4d60ac3621c7563b96a41a3e4023c6c091e43fb0
3C Frg <167719897+3C-FLA09@users.noreply.github.com>
2024-11-12 12:52:54 -0300

    Add files via upload

---
+++ Flash Card/perguntas.js
@@ -0,0 +1,47 @@
+criaCartao(
+    'Tecnologia',
+    'O que é CSS?',
+    'O CSS é uma linguagem de estilização'
+)
+
+criaCartao(
+    'Tecnologia',
+    'O que é JavaScript??',
+    'O JavaScript é uma linguagem de programação'
+)
+
+criaCartao(
+    'Tecnologia',
+    'O que é Index.html?',
+    'O Index.html é o índice que cada endereço da web tem em mecanismos de busca'
+)
+
+criaCartao(
+    'Tecnologia',
+    'O que é Python?',
+    'O Python é uma linguagem de programação amplamente usada em aplicações da Web, desenvolvimento de software, ciência de dados e machine learning (ML)'
+)
+
+criaCartao(
+    'Tecnologia',
+    'O que é Java?',
+    'Java é uma linguagem multiplataforma, orientada a objetos e centrada em rede que pode ser usada como uma plataforma em si'
+)
+
+criaCartao(
+    'Tecnologia',
+    'O que é Ruby?',
+    'Ruby é uma linguagem de programação altamente popular que é usada para muitas coisas, desde o desenvolvimento de aplicativos web até a análise de dados. '
+)
+
+criaCartao(
+    'Tecnologia',
+    'O que é C++?',
+    'Sua sintaxe permite programação de alto nível e oferece controle direto sobre o hardware'
+)
+
+criaCartao(
+    'Tecnologia',
+    'O que é Go?',
+    'Golang, também conhecida como Go, é uma linguagem de programação criada pela Google'
+)
